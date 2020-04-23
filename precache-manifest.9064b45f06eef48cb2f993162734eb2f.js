17ce70134ac23534f6d8ec98ed554946b4049689
Pedro Santos <pedrocarlos.ti@gmail.com>
2020-04-23 19:46:15 -0300

    Updates

---
+++ precache-manifest.9064b45f06eef48cb2f993162734eb2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34ba7d90b90cf434f669803b4c7e708",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6f9b1c8b85f6a69be110",
+    "url": "/monsters-rolodex/static/css/main.ec2e55dd.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f9b1c8b85f6a69be110",
+    "url": "/monsters-rolodex/static/js/main.e5f9e0c9.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
